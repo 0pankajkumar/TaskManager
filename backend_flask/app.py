d9c86be36f11087f5dae0951e7f9d1b53a079d11
Pankaj Kumar <805.bluebell@gmail.com>
2023-03-03 00:06:37 +0530

    flask apis work now

--- backend_flask/app.py
+++ backend_flask/app.py
@@ -1,57 +1,84 @@
-from flask import Flask,render_template,request,redirect,url_for,flash
+import json
+import time
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 import sqlite3 as sql
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route("/")
-@app.route("/index")
+@app.route("/tasks")
 def index():
-    con=sql.connect("db_web.db")
-    con.row_factory=sql.Row
-    cur=con.cursor()
-    cur.execute("select * from users")
-    data=cur.fetchall()
-    return render_template("index.html",datas=data)
+    con = sql.connect("db_tasks.db")
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("select * from tasks;")
+    row_headers = [x[0]
+                   for x in cur.description]  # this will extract row headers
+    data = cur.fetchall()
 
-@app.route("/add_user",methods=['POST','GET'])
+    json_data = []
+    for result in data:
+        json_data.append(dict(zip(row_headers, result)))
+    print(json_data)
+    return jsonify(json_data)
+
+
+@app.route("/add", methods=['POST'])
 def add_user():
-    if request.method=='POST':
-        uname=request.form['uname']
-        contact=request.form['contact']
-        con=sql.connect("db_web.db")
-        cur=con.cursor()
-        cur.execute("insert into users(UNAME,CONTACT) values (?,?)",(uname,contact))
-        con.commit()
-        flash('User Added','success')
-        return redirect(url_for("index"))
-    return render_template("add_user.html")
+    if request.method == 'POST':
+        title = request.form['title']
+        eta = int(request.form['eta'])
+        status = request.form['status']
+        print(title, eta, status)
+        con = sql.connect("db_tasks.db")
+        cur = con.cursor()
+        cur.execute(
+            "insert into tasks(title, eta, status) VALUES(?,?,?);", (title,
+                                                                     eta, status)
+        )
 
-@app.route("/edit_user/<string:uid>",methods=['POST','GET'])
-def edit_user(uid):
-    if request.method=='POST':
-        uname=request.form['uname']
-        contact=request.form['contact']
-        con=sql.connect("db_web.db")
-        cur=con.cursor()
-        cur.execute("update users set UNAME=?,CONTACT=? where UID=?",(uname,contact,uid))
         con.commit()
-        flash('User Updated','success')
-        return redirect(url_for("index"))
-    con=sql.connect("db_web.db")
-    con.row_factory=sql.Row
-    cur=con.cursor()
-    cur.execute("select * from users where UID=?",(uid,))
-    data=cur.fetchone()
-    return render_template("edit_user.html",datas=data)
-    
-@app.route("/delete_user/<string:uid>",methods=['GET'])
-def delete_user(uid):
-    con=sql.connect("db_web.db")
-    cur=con.cursor()
-    cur.execute("delete from users where UID=?",(uid,))
-    con.commit()
-    flash('User Deleted','warning')
-    return redirect(url_for("index"))
-    
-if __name__=='__main__':
-    app.secret_key='admin'
+        print('task Added')
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
+
+@app.route("/update", methods=['POST'])
+def update():
+    if request.method == 'POST':
+        id = request.form['id']
+        field_updated = request.form['update_field']
+        new_value = request.form['update_value']
+
+        con = sql.connect("db_tasks.db")
+        cur = con.cursor()
+        cur.execute(f"update tasks set {field_updated}=? where id=?",
+                    (new_value, id))
+        cur.execute(
+            "insert into audittrial(id, field_updated, new_value, timestamp) VALUES(?,?,?,?)",
+            (id, field_updated, new_value, int(time.time()))
+        )
+        con.commit()
+
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
+
+@app.route("/audittrial/<string:uid>", methods=['GET'])
+def audittrial(uid):
+    con = sql.connect("db_tasks.db")
+    cur = con.cursor()
+    cur.execute("select * from audittrial where id=?", (uid))
+    row_headers = [x[0]
+                   for x in cur.description]  # this will extract row headers
+    data = cur.fetchall()
+
+    json_data = []
+    for result in data:
+        json_data.append(dict(zip(row_headers, result)))
+
+    print(json_data)
+    return jsonify(json_data)
+
+
+if __name__ == '__main__':
+    app.secret_key = 'admin'
     app.run(debug=True)
